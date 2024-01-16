287327f702636380e74c50bf64abb4398050a9a0
HongJeongHwan <154218233+HongJeongHwan@users.noreply.github.com>
2024-01-16 18:16:38 +0900

    Add files via upload

--- pages/02_2단계_map.py
+++ pages/02_2단계_map.py
@@ -30,9 +30,50 @@ df_master1.reset_index(inplace=True)
 ###################################################################################
 # 화면 출력
 ###################################################################################
-st.header('2_1단계 : 대여소 정보를 지도에 시각화')
-st.write(df_master1)
-st.map(df_master1) 
+st.header('2단계 : 대여소 정보를 지도에 시각화')
+
+tab1, tab2 = st.tabs(["2D Map", "3D Map"])
+
+with tab1:
+#    st.header('2_1단계 : 대여소 정보를 지도에 시각화')
+   st.write(df_master1)
+   st.map(df_master1) 
+
+with tab2:
+   st.header("3D Map")
+   chart_data = df_master1.loc[:,['lat','lon']]
+   
+   # 지도 그려주기
+   st.pydeck_chart(pdk.Deck(
+   map_style=None,
+   initial_view_state=pdk.ViewState(
+           latitude=37.51,
+           longitude=126.98,
+           zoom=10.2,
+           pitch=50,
+   ),
+   layers=[
+           pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+           ),
+           pdk.Layer(
+           'ScatterplotLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           get_color='[200, 30, 0, 160]',
+           get_radius=200,
+           ),
+   ],
+   ))
+
+
 '''
 숙제 : 마스터 데이터로는 경도,위도 정보가 있는 대여소의 위치만 표시
         추후에 메인의 대여정보 데이터에 경도,위도 컬럼을 추가하여 데이터 표시 해보자.    
--- pages/03_3단계_weather.py
+++ pages/03_3단계_weather.py
@@ -15,10 +15,24 @@ df_weather.rename(columns={'Temperature(캜)':'Temperature',
                            'Dew point temperature(캜)':'Dew point temperature'},inplace=True)
 
 # Date 컬럼을 Datetime으로 변경
-df_weather['Date'] = pd.to_datetime(df_weather['Date'])
+# df_weather['Date'] = pd.to_datetime(df_weather['Date'])
 
 # Month 컬럼 추가
-df_weather['Month'] = pd.to_datetime(df_weather['Date']).dt.month
+# df_weather['Month'] = pd.to_datetime(df_weather['Date']).dt.month
+
+# 통계데이터프레임 생성
+# Descriptive Statistics
+descriptive_stats = df_weather.describe()
+
+# Seasonal Analysis
+seasonal_analysis = df_weather.groupby('Seasons')['Rented Bike Count'].mean()
+
+#Correlation Analysis
+correlation_matrix = df_weather.iloc[:,1:10].corr()
+
+# Holiday vs. Non-Holiday Analysis
+holiday_analysis= df_weather.groupby('Holiday')['Rented Bike Count'].mean()
+
 
 
 
@@ -28,6 +42,29 @@ df_weather['Month'] = pd.to_datetime(df_weather['Date']).dt.month
 st.header('3단계 : 날씨 정보와 연관성 시각화')
 st.write(df_weather)
 
-'''
-숙제 : 현재 데이터를 대여기록과 연계해.. 날씨데이터와 대여건수를 비교분석해보자.    
-'''
+st.subheader('df_weather.describe()')
+st.write(descriptive_stats)
+
+st.subheader('df_weather.groupby(Seasons)[Rented Bike Count].mean()')
+st.write(seasonal_analysis)
+
+
+fig1, ax1 = plt.subplots()
+
+st.subheader('correlation_matrix 차트')
+sns.heatmap(correlation_matrix,annot=True, cmap='coolwarm',ax=ax1)
+plt.xticks(rotation=45, ha='right')
+plt.title('Correlation Analysis')
+st.pyplot(fig1)
+
+
+st.subheader('holiday_analysis')
+st.write(holiday_analysis)
+
+
+sns.pairplot(df_weather[['Rented Bike Count', 'Temperature', 'Humidity(%)', 'Wind speed (m/s)']],ax=ax1)
+st.pyplot(fig1)
+
+sns.pairplot(df_weather, hue='Rented Bike Count', palette='husl',ax=ax1)
+st.pyplot(fig1)
+
